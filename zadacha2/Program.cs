9df1615fac0f9a48aa4654819f59cf34e7ec198c
LeraPolovinkina111 <leo.plv@bk.ru>
2022-12-01 09:11:49 +0500

    задча2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine("{0}-{1}", input[0], input[2]);
+    }
+
+        
+    
+    
