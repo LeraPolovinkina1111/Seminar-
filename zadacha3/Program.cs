24a8b52701d841161d0fab1cde2bc8e0a6cb185e
LeraPolovinkina111 <leo.plv@bk.ru>
2022-12-01 11:27:48 +0500

    задча3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+while  (a>999)  a=a/10;
+{
+    if (a<100)
+    {
+        Console.WriteLine("число не трехзначное");
+    }
+    else ;
+    
+}
+System.Console.WriteLine(a%10);
+
